a912541d9ecf75449781476a7101a9e09fa6a747
Yuri <baldiniyuri@gmail.com>
2022-09-21 15:27:04 -0300

    created wait for db command and created user model

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    uf = models.CharField(max_length=2)
+    country = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(null=True, blank=True, default=False)
+    is_superuser = models.BooleanField(null=True, blank=True, default=False)
+
