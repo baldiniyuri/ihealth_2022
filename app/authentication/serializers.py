dcfbbab45f9772e80796b8673221ee20762af65a
Yuri <baldiniyuri@gmail.com>
2022-09-22 11:00:02 -0300

    created login, logout and profile picture view

--- app/authentication/serializers.py
+++ app/authentication/serializers.py
@@ -13,3 +13,12 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
     is_staff = serializers.BooleanField(required=False)
     is_superuser = serializers.BooleanField(required=False)
+
+
+class CredentialSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    
+class ProfilePictureSerializers(serializers.Serializer):
+    user_id = serializers.IntegerField(read_only=True)
+    image = serializers.ImageField()
--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import UsersView
+from .views import UsersView, LoginView, Logout, ProfilePictureView
 
 
 urlpatterns = [
-    path('register/', UsersView.as_view()), 
+    path('register/', UsersView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/<int:user_id>/', Logout.as_view()),
+    path('profile/picture/', ProfilePictureView.as_view()),
+    path('profile/picture/<int:user_id>/', ProfilePictureView.as_view()), 
 ]
