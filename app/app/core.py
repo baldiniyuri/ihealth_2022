0ae269fb807fe7d552b8ee8a81f795e1c1dc3d50
Yuri <yuriabaldini@gmail.com>
2022-10-06 11:05:04 -0300

    added credentials to core, create medic glucose view

--- app/app/core.py
+++ app/app/core.py
@@ -1,5 +1,6 @@
 from rest_framework.authtoken.models import Token
-    
+from authentication.models import User 
+
 
 def CheckUserToken(request, user_id):
     user_id_token = Token.objects.get(user=user_id)
@@ -19,4 +20,38 @@ def DeleteUserToken(user_id):
         token_to_delete.delete()
         return True
 
+    return False
+
+
+def CheckAdmin(user_id):
+    user = User.objects.get(id=user_id)
+    if user.is_superuser:
+        return True
+    return False
+
+
+def CheckMedic(user_id):
+    user = User.objects.get(id=user_id)
+
+    if user.is_medic:
+        return True
+    return False
+
+
+def FindUser(user_id):
+    found = User.objects.filter(id=user_id)
+    if found:
+        return True
+    return False
+
+
+def CheckAdminCredentials(request, user_id):
+    if CheckUserToken(request, user_id) and CheckAdmin(user_id):
+        return True
+    return False
+
+
+def CheckAdminOrMedicCredentials(request, user_id):
+    if CheckUserToken(request, user_id) and CheckAdmin(user_id) or CheckMedic(user_id):
+        return True
     return False
--- app/glucose/views.py
+++ app/glucose/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from .models import Glucose
 from .serializers import GlucoseSerializer
 from authentication.models import User
-from app.core import CheckUserToken
+from app.core import CheckUserToken, FindUser, CheckAdminOrMedicCredentials
 
 
 class GlucoseView(APIView):
@@ -15,9 +15,8 @@ class GlucoseView(APIView):
 
     
     def get(self, request, user_id):
-        found_user = User.objects.filter(pk=user_id)
-
-        if not found_user:
+     
+        if not FindUser(user_id):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         if not CheckUserToken(request, user_id):
@@ -66,3 +65,40 @@ class GlucoseView(APIView):
         serializer = GlucoseSerializer(glucose)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+class GlucoseMedicView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    
+    def get(self, request, user_id, medic_id):
+
+        if not FindUser(user_id) or not FindUser(medic_id):
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if not CheckAdminOrMedicCredentials(request, medic_id):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        min = self.request.query_params.get('min', '')
+        max = self.request.query_params.get('max', '')
+ 
+        if min and max:
+
+            param = "date_time"
+
+            lookup_filter = {}
+            lookup_filter[f'{param}__gte'] = min
+            lookup_filter[f'{param}__lte'] = max
+            
+            glucose = Glucose.objects.filter(**lookup_filter, user=user_id)
+        else:
+            glucose = Glucose.objects.filter(user=user_id)
+        
+        user = User.objects.get(id=user_id)
+        full_name = '{} {}'.format(user.first_name, user.last_name)
+
+        serializer = GlucoseSerializer(glucose, many=True)
+
+        return Response({'user': full_name, 'glucose_level':serializer.data}, status=status.HTTP_200_OK)
