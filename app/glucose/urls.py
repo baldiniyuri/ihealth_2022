9846658ea1782a2d67844c1fc322b9ccab52ae18
Yuri <yuriabaldini@gmail.com>
2022-10-06 11:17:19 -0300

    created medic url for glucose

--- app/glucose/urls.py
+++ app/glucose/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GlucoseView
+from .views import GlucoseView, GlucoseMedicView
 
 
 urlpatterns = [
     path('glucose/', GlucoseView.as_view()),
-    path('glucose/<int:user_id>/', GlucoseView.as_view())
+    path('glucose/<int:user_id>/', GlucoseView.as_view()),
+    path('medic-glucose/<int:user_id>/<int:medic_id>/', GlucoseMedicView.as_view())
 ]
